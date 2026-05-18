5ecec6e0fef3840ed5e29bffa17f2b1a9eebd2b3
StqckzzProjects <kidkd2010@gmail.com>
2026-05-18 17:01:23 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Hermitage Business Group
+// Business Card Template
+
+console.log("Business Card Loaded");
+
+/*
+
+CUSTOMIZATION GUIDE
+
+You can edit:
+
+1. Name
+2. Job Title
+3. Company Name
+4. Colors
+5. Contact Info
+6. Logo Initials
+7. Social Media
+8. Website
+9. Address
+
+Most edits are in:
+- index.html
+- style.css
+
+
+<div class="badge">
+        Customizable Template
+      </div>
+*/
+
